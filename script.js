3ee23dd5ecef57530023453f2296128e6d63ad3c
Tillaeva <tillaevajm@gmail.com>
2021-01-15 18:24:46 +0500

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+var name = prompt('Your Name')
+console.log('My name is', name);
+
+var age = prompt('Your Age')
+console.log('My age is', age);
+
+var sum = prompt('Solve 3 + 9 = ?')
+console.log('Activity 1: 3 + 9 = 12. Your answer is', sum);
+
+var minus = prompt('Solve 18 - 3 = ?')
+console.log('Activity 2: 18 - 3 = 15. Your answer is', minus);
+
+var multiply = prompt('Solve 2 * 6 = ?')
+console.log('Activity 3: 2 * 6 = 12. Your answer is', multiply);
+
+var divide = prompt('Solve 18 / 3 = ?')
+console.log('Activity 4: 18 / 3 = 6. Your answer is', divide);
+
+var ostatok = prompt('Solve 22 % 4 = ?')
+console.log('Activity 5: 22 % 4 = 2. Your answer is', ostatok);
+
+alert('Open console to see the result')
